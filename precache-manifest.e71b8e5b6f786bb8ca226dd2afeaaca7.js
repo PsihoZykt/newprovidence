59c320b93b4c609197cfd6f4aa2b050b62e83214
psihoz <psihozykt@gmail.com>
2020-06-18 19:52:32 +0300

    Updates

---
+++ precache-manifest.e71b8e5b6f786bb8ca226dd2afeaaca7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ca0e585786270cda6ec59ecab6cd22",
+    "url": "/newprovidence/index.html"
+  },
+  {
+    "revision": "f8ab2d99a6f8eda3581f",
+    "url": "/newprovidence/static/css/main.dab942e3.chunk.css"
+  },
+  {
+    "revision": "35fd02dfd7478f29d28d",
+    "url": "/newprovidence/static/js/2.41fdc860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newprovidence/static/js/2.41fdc860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ab2d99a6f8eda3581f",
+    "url": "/newprovidence/static/js/main.a4461331.chunk.js"
+  },
+  {
+    "revision": "0ccf4dec99b0e999ae1c",
+    "url": "/newprovidence/static/js/runtime-main.c915d967.js"
+  },
+  {
+    "revision": "2398484ecfa8c9021c561a4048ee8d16",
+    "url": "/newprovidence/static/media/IphoneScreen.2398484e.png"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/newprovidence/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/newprovidence/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "e8e0c5b5b94043c58ce079d04d34e4eb",
+    "url": "/newprovidence/static/media/Photo.e8e0c5b5.png"
+  },
+  {
+    "revision": "1c2fde75418a3278358f7f9683f7a96e",
+    "url": "/newprovidence/static/media/Photo1.1c2fde75.png"
+  },
+  {
+    "revision": "34d4ff095c449f7dc67bc01cf945a1b4",
+    "url": "/newprovidence/static/media/Photo2.34d4ff09.png"
+  },
+  {
+    "revision": "3b5fb64371b4ff561d87033adaab729b",
+    "url": "/newprovidence/static/media/Screen_1.3b5fb643.png"
+  },
+  {
+    "revision": "c402831e161003f6a0f00d4fa1bf4a22",
+    "url": "/newprovidence/static/media/Screen_2.c402831e.png"
+  },
+  {
+    "revision": "3a7af54d563a26cb2eaa45ef32a1c232",
+    "url": "/newprovidence/static/media/Screen_3.3a7af54d.png"
+  },
+  {
+    "revision": "3a6286b21c05f201ebd44326e1b3bdb6",
+    "url": "/newprovidence/static/media/Screen_4.3a6286b2.png"
+  },
+  {
+    "revision": "159cb67fc3bc762a8c3232f0a0c6728e",
+    "url": "/newprovidence/static/media/VarelaRound-Regular.159cb67f.ttf"
+  },
+  {
+    "revision": "edcfc8647e013dfbbc2f52bea88c5aae",
+    "url": "/newprovidence/static/media/World_Map.edcfc864.png"
+  },
+  {
+    "revision": "e610c259c1b3e6f2f95d1c4f3710ee6f",
+    "url": "/newprovidence/static/media/background.e610c259.png"
+  },
+  {
+    "revision": "6bb49e64b66d67706ac2d190be21951e",
+    "url": "/newprovidence/static/media/iPhone_6.6bb49e64.png"
+  },
+  {
+    "revision": "06c1a592339fca1b09a2594c5da017ff",
+    "url": "/newprovidence/static/media/iphone6.06c1a592.png"
+  }
+]);
