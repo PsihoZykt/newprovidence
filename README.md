13e174472986da64a00b624b1dd9b3b2472ac52d
psihozykt <psihozykt@gmail.com>
2021-12-27 14:10:50 +0900

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# General
+Just a simple page to css practice based on create-react-app (Desktop view only)
+
+# Screenshots
+
+## Desktop
+
+![desktop](./screenshots/newprovidence-desktop.png)
--- src/components/Footer/Links/Links.jsx
+++ src/components/Footer/Links/Links.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './Links.module.css'
 import Social from "./Social/Social";
-import LearnMore from "./LearnMore/LearnMore";
+// import LearnMore from "./LearnMore/LearnMore";
 import AboutUs from "./AboutUs/AboutUs";
 import Support from "./Support/Support";
 
@@ -9,7 +9,7 @@ const Links = () => {
     return (
         <div className={s.linksWrapper}>
             <Social/>
-            <LearnMore/>
+            {/*<LearnMore/>*/}
             <AboutUs/>
             <Support/>
         </div>
